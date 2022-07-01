4173c0addf6502a429b297e53330714b203cfdc3
Kotaro Kuroda <f1004.kuro@gmail.com>
2022-07-01 13:47:09 +0900

    Update retinanet.py

--- retinanet.py
+++ retinanet.py
@@ -1,7 +1,44 @@
-import torchvision
+from torchvision.models.detection.retinanet import retinanet_resnet50_fpn_v2, RetinaNet, RetinaNetHead
+from torchvision.models.detection.rpn import AnchorGenerator
+import poolformer
+from torchvision.models.detection.backbone_utils import BackboneWithFPN
+from torchvision.ops.feature_pyramid_network import LastLevelP6P7
+from functools import partial
+from torch import nn
 
 
-def model(dataset_class):
-    model = torchvision.models.detection.retinanet_resnet50_fpn(
-        pretrained_backbone=True, num_classes=len(dataset_class) + 1)
+def get_model(num_classes):
+    anchor_sizes = tuple((x, int(x * 2 ** (1.0 / 3)), int(x * 2 ** (2.0 / 3)))
+                         for x in [16, 32, 64, 128, 256])
+    aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+    anchor_generator = AnchorGenerator(
+        anchor_sizes, aspect_ratios
+    )
+    model = retinanet_resnet50_fpn_v2(num_classes=num_classes,
+                                      pretrained=False, pretrained_backbone=True, progress=True, topk_candidates=1000, detections_per_img=500)
+    model.anchor_generator = anchor_generator
+    return model
+
+
+def poolformer_backbone_model(num_classes):
+    plformer = poolformer.poolformer_s36(fork_feat=True)
+    return_layers = {f'norm{k}': v for v, k in enumerate([0, 2, 4, 6])}
+    in_channles_list = [64, 128, 320, 512]
+    out_channels = 256
+    backbone = BackboneWithFPN(plformer, return_layers, in_channles_list, out_channels, extra_blocks=LastLevelP6P7(512, 256))
+    backbone.body = plformer
+    anchor_sizes = tuple((x, int(x * 2 ** (1.0 / 3)), int(x * 2 ** (2.0 / 3)))
+                         for x in [16, 32, 64, 128, 256, 512])
+    aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+    anchor_generator = AnchorGenerator(
+        anchor_sizes, aspect_ratios
+    )
+    head = RetinaNetHead(
+        backbone.out_channels,
+        anchor_generator.num_anchors_per_location()[0],
+        num_classes,
+        norm_layer=partial(nn.GroupNorm, 32),
+    )
+    head.regression_head._loss_type = "giou"
+    model = RetinaNet(backbone, num_classes=num_classes, anchor_generator=anchor_generator, head=head, topk_candidates=1000, detections_per_img=500)
     return model
